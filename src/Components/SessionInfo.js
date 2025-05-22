ec14b137ff3e327327579035f253295f2cfccc66
Ayleen <ayleenpiteotarpy@gmail.com>
2025-05-21 22:04:33 -0700

    download/save buttons

--- src/Components/SessionInfo.js
+++ src/Components/SessionInfo.js
@@ -6,7 +6,17 @@ function RecordingInfo() {
     <div className="session-info">
       <RecordingSuite />
       <input type="text" className="title-box" placeholder="recording title" name='title' required />
-      <div></div>
+      
+      <div className="file">
+        <button className="file-button">
+          <div className="file-text">Save</div>
+          <i className="file-icon fa-solid fa-cloud"></i>
+        </button>
+        <button className="file-button">
+          <div className="file-text">Download</div>
+          <i className="file-icon fa-solid fa-floppy-disk"></i>
+        </button>
+      </div>
     </div>
   );
 }
