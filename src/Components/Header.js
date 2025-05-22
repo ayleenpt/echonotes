16a66dd17bb620318a9184678ef1e61fca2205bd
Ayleen <ayleenpiteotarpy@gmail.com>
2025-05-21 18:43:45 -0700

    add header

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import '../Styles/Header.css'
+
+function Header() {
+  return (
+    <div className="header">
+      <i class="fa-solid fa-bars"></i>
+      <i className="fa-solid fa-circle-user"></i>
+    </div>
+  );
+}
+
+export default Header;
